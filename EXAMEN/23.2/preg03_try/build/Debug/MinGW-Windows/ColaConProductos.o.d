2e08f9fb89c7bb993559de779615888290d071d7
mell <131613292+Meluca17@users.noreply.github.com>
2024-05-14 06:19:44 -0500

    Add files via upload

---
+++ EXAMEN/23.2/preg03_try/build/Debug/MinGW-Windows/ColaConProductos.o.d
@@ -0,0 +1,6 @@
+build/Debug/MinGW-Windows/ColaConProductos.o: ColaConProductos.cpp \
+ ColaGenerica.h ColaConProductos.h
+
+ColaGenerica.h:
+
+ColaConProductos.h:
---
+++ EXAMEN/23.2/preg03_try/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,14 @@
+build/Debug/MinGW-Windows/main.o: main.cpp \
+ Pregunta01Examen01PunterosMultiples.h \
+ Pregunta02Examen01PunterosGenericos.h ColaConEnteros.h ColaGenerica.h \
+ ColaConProductos.h
+
+Pregunta01Examen01PunterosMultiples.h:
+
+Pregunta02Examen01PunterosGenericos.h:
+
+ColaConEnteros.h:
+
+ColaGenerica.h:
+
+ColaConProductos.h:
