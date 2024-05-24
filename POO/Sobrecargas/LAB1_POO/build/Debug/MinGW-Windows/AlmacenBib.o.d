4962f64d54e81a7f379c71b09077b790dda4f3e6
mell <131613292+Meluca17@users.noreply.github.com>
2024-05-24 01:36:14 -0500

    Add files via upload

---
+++ POO/Sobrecargas/LAB1_POO/build/Debug/MinGW-Windows/AlmacenBib.o.d
@@ -0,0 +1,10 @@
+build/Debug/MinGW-Windows/AlmacenBib.o: AlmacenBib.cpp AlmacenBib.h \
+ Cliente.h LibroSolicitado.h Libro.h
+
+AlmacenBib.h:
+
+Cliente.h:
+
+LibroSolicitado.h:
+
+Libro.h:
---
+++ POO/Sobrecargas/LAB1_POO/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,10 @@
+build/Debug/MinGW-Windows/main.o: main.cpp Libro.h AlmacenBib.h Cliente.h \
+ LibroSolicitado.h
+
+Libro.h:
+
+AlmacenBib.h:
+
+Cliente.h:
+
+LibroSolicitado.h:
